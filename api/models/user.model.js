1dda7f5adf1d4715bdf2eff305b25642c268edf0
mraiyuu <munenedenis618@gmail.com>
2024-02-19 06:53:19 +0300

    routes and auth controller added, backend and frontend connected

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -3,18 +3,18 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     username: {
-      type: "string",
-      required: "true",
-      unique: "true",
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-      type: "string",
-      required: "true",
-      unique: "true",
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-      type: "string",
-      required: "true",
+      type: String,
+      required: true,
     },
   },
   { timestamps: true }
