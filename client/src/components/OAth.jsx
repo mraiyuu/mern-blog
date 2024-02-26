713628804af0346e3a0c9d05dc28e988d8305507
mraiyuu <munenedenis618@gmail.com>
2024-02-26 23:24:52 +0300

    add Google OAuth functionality

--- client/src/components/OAth.jsx
+++ client/src/components/OAth.jsx
@@ -1,23 +1,22 @@
-import React from "react";
 import { Button } from "flowbite-react";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 import { app } from "../firebase";
 import { useDispatch } from "react-redux";
-import { signInSucess } from "../redux/user/userSlice";
-import { useNavigate } from "react-router-dom";
 
-const OAth = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+import { useNavigate } from "react-router-dom";
+import { signInSucess } from "../redux/user/userSlice";
+
+export default function OAuth() {
   const auth = getAuth(app);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
     const provider = new GoogleAuthProvider();
     provider.setCustomParameters({ prompt: "select_account" });
-
     try {
       const resultsFromGoogle = await signInWithPopup(auth, provider);
-      const res = fetch("/api/auth/google", {
+      const res = await fetch("/api/auth/google", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -35,7 +34,6 @@ const OAth = () => {
       console.log(error);
     }
   };
-
   return (
     <Button
       type="button"
@@ -47,6 +45,4 @@ const OAth = () => {
       Continue with Google
     </Button>
   );
-};
-
-export default OAth;
+}
