f991de49416d8700d2e61d54dc51bb629259b787
mraiyuu <munenedenis618@gmail.com>
2024-03-01 21:05:06 +0300

    auth controller refactor

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    console.log(tabFromUrl);
+  }),
+    [location.search];
+  return <div>Dashboard</div>;
 }
